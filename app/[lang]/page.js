0c0d2e454e6cbe4be11a4044f5743d4d631a683e
kolomoitsev.m <kolomoitsev.m@gmail.com>
2025-10-23 23:11:55 +0700

    hero mobile

--- app/[lang]/page.js
+++ app/[lang]/page.js
@@ -105,31 +105,32 @@ export default function Home({ params }) {
       </div>
 
       <div className="hero-section">
+        
         <div className="hero-background-image">
           <img src="/content/home_main.PNG" alt="author" />
         </div>
         <div className="hero-overlay">
           <div className="hero-content">
+            <div className="hero-video-frame">
+              <div className="iphone-frame">
+                <div className="iphone-notch"></div>
+                <video
+                  className="iphone-video"
+                  autoPlay
+                  loop
+                  muted
+                  playsInline
+                >
+                  <source src="/content/presentation.MOV" type="video/mp4" />
+                </video>
+              </div>
+            </div>
             <h1 className="hero-name" dangerouslySetInnerHTML={{ __html: t('home.heroName') }}></h1>
             <p className="hero-tagline">{t('home.description')}</p>
             <div className="hero-buttons">
               <button className="hero-btn hero-btn-primary">
                 {t('home.auth.registerBtn')}
               </button>
-            </div>
-          </div>
-          <div className="hero-video-frame">
-            <div className="iphone-frame">
-              <div className="iphone-notch"></div>
-              <video
-                className="iphone-video"
-                autoPlay
-                loop
-                muted
-                playsInline
-              >
-                <source src="/content/presentation.MOV" type="video/mp4" />
-              </video>
             </div>
           </div>
         </div>
